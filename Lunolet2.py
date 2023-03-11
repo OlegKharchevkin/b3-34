e724df492c0b3751d0828487c061bc5f2646e9a0
Oleg <harchevknoleg@outlook.com>
2023-03-11 23:02:47 +0300

    class created

---
+++ Lunolet2.py
@@ -0,0 +1,12 @@
+class Lunolet2():
+    def __init__(self, output_ = print, input_ = input, r = ([0]*13), config_ = True):
+        pass
+    def __call__():
+        pass
+    def __config():
+        pass
+    def __step():
+        pass
+if __name__ == "__main__":
+    l2 = Lunolet2()
+    l2()
