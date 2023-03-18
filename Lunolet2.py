2fe97af3c0a08d019c5c7cb8b5efb0e2d675340f
Oleg <harchevknoleg@outlook.com>
2023-03-18 14:18:37 +0300

    input/output

--- Lunolet2.py
+++ Lunolet2.py
@@ -1,7 +1,7 @@
 import math
 class Lunolet2():
-    r_default = [0,       # 0  режим таймера
-                 0,       # 1  масса сжигаемого топлива
+    r_default = [0,       # 0  горизонтальная скорость
+                 0,       # 1  наклон корабля
                  0,       # 2  время манёвра
                  0,       # 3  ускорение корабля
                  1.62,    # 4  ускорение свободного падения
@@ -10,48 +10,64 @@ class Lunolet2():
                  9.81 * 3,# 7  предельное ускорение
                  0,       # 8  расход топлива
                  0,       # 9  высота корабля
-                 0,       # 10 скорость корабля
-                 3600,    # 11 оставшееся время
+                 0,       # 10 вертикальная скорость
+                 250000,  # 11 растояние до цели
                  400]     # 12 запас топлива
     def __init__(self, output_ = print, input_ = input, r = ([0]*13), config_ = True):
-        pass
-    def __call__():
-        pass
+        self.output = output_
+        self.input = input_
+        self.r = r
+        if config_: self.__config()
+    def __call__(self):
+        while True:
+            self.output(f"Верт. скорость: {self.r[10]}")
+            self.output(f"Высота:         {self.r[9]}")
+            self.output(f"Гор. скорость:  {self.r[0]}")
+            self.output(f"Растояние:      {self.r[11]}")
+            self.output(f"Запас топлива:  {self.r[11]}")
+            self.output(f"Ускорение:      {self.r[3]}")
+            self.output(f"Время:          {self.r[12]}")
+            while (x := float(self.input())) >= self.r[5] * 0.05:
+                self.output("Слишком большой расход!")
+            while (y := float(self.input())) == 0:
+                self.output("Время не может равняться нулю!")
+            z = float(self.input())
+            self.__step(x, y, z)
     def __config():
         pass
     def __step(self, x, y, z):
-        r1 = z
-        r2 = y
-        r8 = x / r2
-        r3 = (r8 * r6) / (rd + r5)
+        self.r[1] = z
+        self.r[2] = y
+        self.r[8] = x / self.r[2]
+        self.r[3] = (self.r[8] * self.r[6]) / (self.r[12] + self.r[5])
         while True:
             while True:
-                r0tmp = r0
-                r0 += math.sin(r1) * r3 * r2
-                rc -= ((r0tmp + r0) * 2) / 2
-                rbtmp = rb
-                rb -= (r4 - r3 * math.cos(r1)) * r2
-                ra += ((rbtmp+rb) * 2) / 2
-                rd -= r8 * r2
-                if rd >=0:
+                r0tmp = self.r[0]
+                self.r[0] += math.sin(self.r[1]) * self.r[3] * self.r[2]
+                self.r[11] -= ((r0tmp + self.r[0]) * 2) / 2
+                rbtmp = self.r[10]
+                self.r[10] -= (self.r[4] - self.r[3] * math.cos(self.r[1])) * self.r[2]
+                self.r[9] += ((rbtmp+self.r[10]) * 2) / 2
+                self.r[12] -= self.r[8] * self.r[2]
+                if self.r[12] >=0:
                     break
-                r2 = rd / r8
-            if ra < 0:
-                r2 = (2 * ra) / ((2 * ra * (r4 - r3 * math.cos(r1)))**0.5 - rb)
-            elif ra != 0:
-                if r3 < r7:
-                    if rd == 0:
+                self.r[2] = self.r[12] / self.r[8]
+            if self.r[9] < 0:
+                self.r[2] = (2 * self.r[9]) / ((2 * self.r[9] * (self.r[4] - self.r[3] * math.cos(self.r[1])))**0.5 - self.r[10])
+            elif self.r[9] != 0:
+                if self.r[3] < self.r[7]:
+                    if self.r[12] == 0:
                         self.output("Нехватка топлива!")
-                        r2 = r6
-                        r8 = 0
-                        r3 = 0
+                        self.r[2] = self.r[6]
+                        self.r[8] = 0
+                        self.r[3] = 0
                     else:
                         break #ввод/вывод
                 else:
                     self.output("Перегрузки!")
-                    r2 = r3 - r7
-                    r8 = rd / r2
-                    r3 = (r8 * r6) / (rd + r5)
+                    self.r[2] = self.r[3] - self.r[7]
+                    self.r[8] = self.r[12] / self.r[2]
+                    self.r[3] = (self.r[8] * self.r[6]) / (self.r[12] + self.r[5])
             else:
                 break #ввод/вывод
             
